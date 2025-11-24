c83ce3407f5b2eb5f801a93b4b96742b57f7d445
OUSSAMA NAYA <oussamanaya8@gmail.com>
2025-11-24 16:44:44 +0100

    add routerLink to our home componant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// add the moduls to app.moduls
 import { HomeModule } from './home/home.module';
 import { Reservation } from './models/reservation';
 
@@ -13,7 +14,7 @@ import { Reservation } from './models/reservation';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule  // add the RoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
