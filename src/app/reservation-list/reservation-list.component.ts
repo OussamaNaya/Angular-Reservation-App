ebb5e09ced040b5d4c66e65599fd58a1720c5d35
OUSSAMA NAYA <oussamanaya8@gmail.com>
2025-11-25 23:24:02 +0100

    create the list of reservations component

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationService } from '../reservation/reservation.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+
+export class ReservationListComponent implements OnInit{
+    reservations: Reservation[] = [];
+
+    // Inject the ReservationService.
+    constructor(private reservationService : ReservationService){
+    }
+    
+    ngOnInit(): void {
+      this.reservations = this.reservationService.getReservations();
+      
+      console.log("ngOnInit => ")
+      for (let r of this.reservations) {
+        console.log(r);
+      }
+    }  
 
 }
